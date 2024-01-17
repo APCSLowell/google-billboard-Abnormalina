6e2f979a70e7f0072472857e665274b05e6102bd
Alina liu <119249419+Abnormalina@users.noreply.github.com>
2024-01-17 09:38:14 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,15 +1,31 @@
-
-
-public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public static void main(final String[] args) {
-		System.out.println("Replace this string with your answer!");
-	}
-	
-
-	//Finish this function
-	public boolean isPrime(double d){
-	}
-
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+public void setup()  
+{            
+  
+  for(int k = 1; k < e.length()- 10;k++){
+    String ten = e.substring(k, k+10);
+     double num = Double.parseDouble(ten);
+   
+if(isPrime(num)) {
+      System.out.println(ten + "is prime");
+      break;
+    }
+}
+}
+public void draw()  
+{   
+  //not needed for this assignment
+}  
+public boolean isPrime(double dNum)  
+{   
+   if (dNum < 2){
+    return false;
+  }
+  for(int i = 2; i <= Math.sqrt(dNum); i++){
+    if(dNum %i == 0) {
+      return false;
+    }
+  }  
+    return true;  
 }
 
