8baa04669202f42114a7a46fa530f8289bfb0fd9
Alina liu <119249419+Abnormalina@users.noreply.github.com>
2024-01-18 18:28:50 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,31 +1,32 @@
-String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-public void setup()  
-{            
-  
-  for(int k = 1; k < e.length()- 10;k++){
-    String ten = e.substring(k, k+10);
-     double num = Double.parseDouble(ten);
-   
-if(isPrime(num)) {
-      System.out.println(ten);
-      break;
+public class GoogleBillboard {
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
+public static void main(final String[] args) {
+  int ten = 10;
+  int index = 1;
+  boolean primeFound = false;
+
+while ((!primeFound) && index + ten <= e.length()) {
+        String substring = e.substring(index, index + ten);
+        double parsedDouble = Double.parseDouble(substring);
+
+    if (isPrime(parsedDouble)) {
+                  System.out.println(parsedDouble);
+                  primeFound = true;
+              } 
+
+              index++;
+       }
+
     }
-}
-}
-public void draw()  
-{   
-  //not needed for this assignment
-}  
-public boolean isPrime(double dNum)  
-{   
-   if (dNum < 2){
+public static boolean isPrime(double num){
+  if (num < 2){
     return false;
   }
-  for(int i = 2; i <= Math.sqrt(dNum); i++){
-    if(dNum %i == 0) {
-      return false;
-    }
-  }  
-    return true;  
+  for(int i=2; i<=Math.sqrt(num); i++){
+    if(num%i==0){
+  return false;
+  }
+  }
+  return true;
 }
-
+}
